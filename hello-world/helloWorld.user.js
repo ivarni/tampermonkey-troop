9582ea9295449b2a4196532fb68e48749ad8e825
Ivar Nilsen <post@ivarnilsen.com>
2015-06-22 15:08:55 +0200

    Update helloWorld.user.js

--- hello-world/helloWorld.user.js
+++ hello-world/helloWorld.user.js
@@ -2,7 +2,6 @@
 // @name         Hello world
 // @version      0.1
 // @match        *://*/*
-// @version      1.0
 // @downloadUrl  https://github.com/ivarni/tampermonkey-troop/raw/master/hello-world/helloWorld.user.js
 // ==/UserScript==
 
