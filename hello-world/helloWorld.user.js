03859441243040c2682f4b9933b6c3d67d508e7c
Ivar Nilsen <ivar.nilsen@sparebank1.no>
2015-06-22 15:07:47 +0200

    Metadata for updating

--- hello-world/helloWorld.user.js
+++ hello-world/helloWorld.user.js
@@ -2,6 +2,8 @@
 // @name         Hello world
 // @version      0.1
 // @match        *://*/*
+// @version      1.0
+// @downloadUrl  https://github.com/ivarni/tampermonkey-troop/raw/master/hello-world/helloWorld.user.js
 // ==/UserScript==
 
 console.log('Hello world');
