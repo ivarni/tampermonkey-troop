1c8610ced2c5694dce75a59655d702bb6282d829
Ivar Nilsen <post@ivarnilsen.com>
2015-08-01 10:09:42 +0200

    Get rid of nagging banner

---
+++ vg-bannerblock/vgBannerblock.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         VG: Block ugly top-banner
+// @description  Does what it says on the tin
+// @version      0.1
+// @match        *://www.vg.no/*
+// @grant        none
+// @downloadUrl  https://github.com/ivarni/tampermonkey-troop/raw/master/vg-bannerblock/vgBannerblock.user.js
+// ==/UserScript==
+
+window.__AB__ = function() {};
