71e5fdfac12af3cf2cc17dcabfdcce63de3964c9
Ivar Nilsen <post@ivarnilsen.com>
2015-08-01 09:16:40 +0200

    Remove example

--- hello-world/helloWorld.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Hello world
-// @version      0.3
-// @match        *://*/*
-// @grant        none
-// @downloadUrl  https://github.com/ivarni/tampermonkey-troop/raw/master/hello-world/helloWorld.user.js
-// ==/UserScript==
-
-console.log('Hello autoupdated world!');
